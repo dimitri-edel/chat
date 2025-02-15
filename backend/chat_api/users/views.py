85280ced0b637ee63a14dc62483fa6dd3fd63ef4
dimitri-edel <dimitri.edel@gmx.net>
2025-02-15 11:34:16 +0100

    Add UserListView to views in users app

--- backend/chat_api/users/views.py
+++ backend/chat_api/users/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+from .models import UserStatus
+from .models import Invitation
 
-# Create your views here.
+class UserListView(generics.ListAPIView):
+    ''' Get a list of all users including their online status '''
+
+    queryset = User.objects.all()
+
+    def list(self, request):
+        queryset = self.get_queryset()
+        user_statuses = UserStatus.objects.all()
+        users_data = []
+        for user in queryset:
+            user_status = user_statuses.filter(user=user).first()
+            users_data.append({
+                "user_id": user.id,
+                "username": user.username,
+                "online": user_status.online if user_status else False
+            })
+        return Response(users_data)
+
