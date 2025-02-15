2a319b485d8d8893783aa9789829cffc0e58f434
dimitri-edel <dimitri.edel@gmx.net>
2025-02-15 11:30:52 +0100

    Add models to app users

--- backend/chat_api/users/models.py
+++ backend/chat_api/users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from user_channels.models import Channel
 
+class UserStatus(models.Model):
+    user = models.OneToOneField('auth.User', on_delete=models.CASCADE)
+    online = models.BooleanField(default=False)
+    
 # Create your models here.
+class Invitation(models.Model):
+    channel = models.ForeignKey(Channel, on_delete=models.CASCADE)
+    sender = models.ForeignKey('auth.User', related_name='sent_invitations', on_delete=models.CASCADE)
+    receiver = models.ForeignKey('auth.User', related_name='received_invitations', on_delete=models.CASCADE)
+    accepted = models.BooleanField(default=False)
+
+    class Meta:
+        unique_together = ('sender', 'receiver')
