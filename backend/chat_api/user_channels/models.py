1e4a0165b6f4c9f610ba04cf3b957503b17fbada
dimitri-edel <dimitri.edel@gmx.net>
2025-02-15 11:28:24 +0100

    Add models to usre_channels

--- backend/chat_api/user_channels/models.py
+++ backend/chat_api/user_channels/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Channel(models.Model):
+    name = models.CharField(max_length=255)
+    users = models.ManyToManyField('auth.User', related_name='user_channels')
+
+class Message(models.Model):
+    Channel = models.ForeignKey(Channel, on_delete=models.CASCADE)
+    sender = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
